153e0089a7fe59376f3f2fb91c8363ce9dc921ef
kennethAsher <2573334038@qq.com>
2020-04-09 13:26:44 +0800

    田建新文件

---
+++ new_clean/民事/clean_lawyer_judge.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+# @Time    : 2020/4/9 1:10 下午
+# @Author  : kennethAsher
+# @Email   : 1131771202@qq.com
+# @Content : 在民事案件中清洗出律师，律所，审判人员以及合作律师对售后律师
+'''
+
+
