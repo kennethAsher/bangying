a253f13ab24d7af9a6ff1fc3c3b88b82bf572183
kennethAsher <2573334038@qq.com>
2020-04-23 10:23:36 +0800

    添加了docid字段

--- new_clean/handler/judge_handler/judge_court_level.py
+++ new_clean/handler/judge_handler/judge_court_level.py
@@ -3,7 +3,7 @@
 @fole   : judge_court_level.py
 @ctime  : 2020/4/22 19:22
 @Email  : 1131771202@qq.com
-@content: 将审判人员按照法院层级分类，并且最终保留层级，日期，法院名称字段
+@content: 将审判人员按照法院层级分类，并且最终保留层级，日期，法院名称以及文书的位移标识id字段
 """
 
 import os
@@ -31,12 +31,13 @@ class judge_court_level():
                 fields = line.strip().split('|')
                 if len(fields)<3:
                     continue
-                judge_name = fields[0]
-                court_name = fields[1]
-                year = fields[2]
-                date = fields[3]
-                level = fields[8]
-                out_line = judge_name+'|'+court_name+'|'+year+'|'+date+'|'+level+'\n'
+                doc_id = fields[0]
+                judge_name = fields[1]
+                court_name = fields[2]
+                year = fields[3]
+                date = fields[4]
+                level = fields[9]
+                out_line = doc_id+'|'+judge_name+'|'+court_name+'|'+year+'|'+date+'|'+level+'\n'
 
                 if '最高' in level:
                     write_level_3.write(out_line)
